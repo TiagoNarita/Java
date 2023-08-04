b191b5990ba0ec469322c4a52d98f45a16baf4ac
Tiago Narita <132394947+TiagoNarita@users.noreply.github.com>
2023-08-03 23:53:20 -0300

    evolve principalComBusca.java

--- aluraJava-poo/principalComBusca.java
+++ aluraJava-poo/principalComBusca.java
@@ -1,20 +1,35 @@
 package br.com.alura.screenmatch.main;
 
+import br.com.alura.screenmatch.modelos.Titulo;
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class principalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite o filme para busca");
+        var busca = leitura.nextLine();
+
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=53a5d6af";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=53a5d6af"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(response.body());
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new Gson();
+        Titulo meutitulo = gson.fromJson(json, Titulo.class);
+        System.out.println(meutitulo);
     }
 }
