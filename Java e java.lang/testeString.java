ba6b0ac2d4aa8a678de77cd82d073f770f63b3b5
Tiago Narita <132394947+TiagoNarita@users.noreply.github.com>
2023-09-05 16:53:53 -0300

    Create testeString.java

---
+++ Java e java.lang/testeString.java
@@ -0,0 +1,13 @@
+package com.example.javpacotes;
+
+public class testeString {
+    public static void main(String[] args) {
+        String nome = "Alura";
+
+        String outra = nome.replace("A", "a");
+        System.out.println(outra);
+
+        //nome.toLowerCase();
+        System.out.println(nome);
+    }
+}
